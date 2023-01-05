f18aa9fca10038997e274c71cdfae846ffaf94e9
Eulier <eulier@toolboxos.com>
2023-01-05 19:31:38 -0300

    modular graphql server using graphql-tools package

---
+++ products/products.model.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        id: 'redshoe',
+        description: 'Red Shoe',
+        price: 42.42
+      },
+      {
+        id: 'bluejeans',
+        description: 'blue jeans',
+        price: 55.55
+      }
+]
--- server.js
+++ server.js
@@ -1,66 +1,18 @@
+const path = require('path')
 const express = require('express')
-const { buildSchema } = require('graphql')
 const { graphqlHTTP } = require('express-graphql')
 
-const schema = buildSchema(`
-  type Query {
-    products: [Product]
-    orders: [Order]
-  }
+const { loadFilesSync } = require('@graphql-tools/load-files')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
 
-  type Product {
-    id: ID!
-    description: String!
-    reviews: [Review]
-    price: Float!
-  }
-
-  type Review {
-    rating: Int!
-    comment: String
-  }
-
-  type Order {
-    date: String!
-    subtotal: Float!
-    items: [OrderItem]
-  }
-
-  type OrderItem {
-    product: Product!
-    quantity: Int!
-  }
-`)
+const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'))
+const schema = makeExecutableSchema({
+  typeDefs: typesArray
+})
 
 const rootValue = {
-  products: [
-    {
-      id: 'redshoe',
-      description: 'Red Shoe',
-      price: 42.42
-    },
-    {
-      id: 'bluejeans',
-      description: 'blue jeans',
-      price: 55.55
-    }
-  ],
-  orders: [
-    {
-      date:'2005-05-05',
-      subtotal: 90.22,
-      items: [
-        {
-          product: { 
-            id: 'redshoe',
-            description: 'Old red shoe',
-            price: 45.11
-          },
-          quantity: 2
-        }
-      ]
-    }
-  ]
+  products: require('./products/products.model'),
+  orders: require('./orders/orders.models')
 }
 
 const app = express()
